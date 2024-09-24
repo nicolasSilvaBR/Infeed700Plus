7624ffab88a6d9e7ecd43682f58a2ff96aa6c9c1
ndasilva <contabit.nicolas@gmail.com>
2024-09-24 12:53:51 +0100

    Commented Code

--- leftMenu.py
+++ leftMenu.py
@@ -1,38 +1,48 @@
-import streamlit as st
-import pandas as pd
-from databaseConnection import mydb
-import streamlit.components.v1 as components
+import streamlit as st  # Importing Streamlit for building the web application
+import pandas as pd  # Importing pandas for data manipulation and analysis
+from databaseConnection import mydb  # Importing the database connection module
+import streamlit.components.v1 as components  # Importing Streamlit components for custom functionalities
 
-# Establish database connection
+# Establish a connection to the database using a custom function from the databaseConnection module
 engine = mydb()
 
-# Credenciais
-ipAddress = "10.202.2.22"
-port = "80"
-database = "Infeed700"
-ReportServerName = "ReportServer"
-username = "icm\\ndasilva"
-password = "1984Icm022*"
-reportRDLname = "Intake"
+# Database credentials and configuration settings
+ipAddress = "10.202.2.22"  # The IP address of the database server
+port = "80"                 # The port number to connect to the database
+database = "Infeed700"     # The name of the database to be accessed
+ReportServerName = "ReportServer"  # The name of the report server
+username = "icm\\ndasilva" # Username for database authentication
+password = "1984Icm022*"   # Password for database authentication
+reportRDLname = "Intake"   # The name of the report to be generated
 
+# Dictionary to map report types to their corresponding header values
 headers = {"Intake": 1, "Blending": 2, "Press": 3}
-reports = {"Intake": 1, "IntakeTip": 1, "Blending": 2,"Press Summary": 3}
+# Dictionary to map specific reports to their corresponding values
+reports = {"Intake": 1, "IntakeTip": 1, "Blending": 2, "Press Summary": 3}
 
 def LeftMenu():
-    # Carregar o arquivo CSS
+    # Function to create a left sidebar menu for report navigation
+
+    # Load a CSS file for custom styling of the Streamlit app
     def local_css(file_name):
+        # Open and read the specified CSS file
         with open(file_name) as f:
+            # Inject the CSS styles into the Streamlit app using markdown
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-    # Chamar a função para carregar o CSS
+    # Call the local_css function to load styles from "style.css"
     local_css("style.css")
 
-
+    # Create a sidebar for navigation
     with st.sidebar:
+        # Iterate over the headers dictionary to create expandable sections in the sidebar
         for header, valor in headers.items():
-            with st.expander(header, expanded=False):
+            with st.expander(header, expanded=False):  # Create an expandable section for each header
+                # Iterate over the reports dictionary to list reports under the corresponding header
                 for report, valor2 in reports.items():
+                    # Check if the report's value matches the current header's value
                     if valor2 == valor:
+                        # Create a clickable link for the report using HTML within markdown
                         st.markdown(f"""
                             <a target="_self" class="sidebar-item" href="http://{ipAddress}:{port}/{ReportServerName}/Pages/ReportViewer.aspx?%2f{database}%2f{report}&rs:Command=Render">{report} <span>›</span></a>
-                        """, unsafe_allow_html=True)
+                        """, unsafe_allow_html=True)  # Render the report link, allowing HTML rendering
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import streamlit as st
-from leftMenu_custom_css import leftMenu_custom_css  # Custom CSS for the left menu
+import streamlit as st  # Importing Streamlit for building the web application
 from leftMenu import LeftMenu  # Import the left menu functionality
-import requests
-from requests_ntlm import HttpNtlmAuth  # Autenticação NTLM
+import requests  # Importing the requests library for making HTTP requests
+from requests_ntlm import HttpNtlmAuth  # Import NTLM authentication support
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")  # Setting the page layout to wide for better space utilization
 
 # Display the left menu
 LeftMenu()
@@ -12,39 +11,39 @@ LeftMenu()
 # Call the intake page with predefined dates
 # intake_page('2024-08-01', '2024-09-17')
 
-# Credenciais
-ipAddress = "10.202.2.22"
-port = "80"
-database = "Infeed700"
-ReportServerName = "ReportServer"
-username = "icm\\ndasilva"
-password = "1984Icm022*"
-reportRDLname ="Intake"
+# Database credentials and configuration settings
+ipAddress = "10.202.2.22"  # The IP address of the database server
+port = "80"                 # The port number to connect to the database
+database = "Infeed700"     # The name of the database to be accessed
+ReportServerName = "ReportServer"  # The name of the report server
+username = "icm\\ndasilva" # Username for database authentication
+password = "1984Icm022*"   # Password for database authentication
+reportRDLname = "Intake"   # The name of the report to be generated
 
+# SSRS URL parameter reference:
 # https://learn.microsoft.com/en-us/sql/reporting-services/url-access-parameter-reference?view=sql-server-ver16
 # rs:: Targets the report server.
 # rc:: Targets an HTML Viewer.
 # rv:: Targets the Report Viewer web part.
 
-# URL do SSRS com parâmetros codificados
-#ssrs_url = f"http://{ipAddress}:{port}/{ReportServerName}/Pages/ReportViewer.aspx?%2f{database}%2f{reportRDLname}&ShowReportParameters=False&rs:Command=Render"
+# Construct the URL for the SSRS report with encoded parameters
 ssrs_url = f"http://{ipAddress}:{port}/{ReportServerName}/Pages/ReportViewer.aspx?%2f{database}%2f{reportRDLname}&rs:Command=Render"
 
-# Faz a requisição usando autenticação NTLM
+# Make the request using NTLM authentication
 response = requests.get(ssrs_url, auth=HttpNtlmAuth(username, password))
 
-# Verifica se a requisição foi bem-sucedida
+# Check if the request was successful
 if response.status_code == 200:
-    # Define a URL com o parâmetro de incorporação do SSRS
+    # Define the URL with the embedding parameter for SSRS
     report_url = f"{ssrs_url}&rs:Embed=true&rc:Parameters=Collapsed"    
 
-    # Código HTML para incorporar o iframe
+    # HTML code to embed the report in an iframe
     iframe_code = f"""
     <iframe width="100%" height="100%" style="min-height: 120vh;" src="{report_url}" frameborder="0" allowfullscreen></iframe>
     """
 
-   # Renderiza o iframe no Streamlit
+    # Render the iframe in Streamlit
     st.components.v1.html(iframe_code, height=800, scrolling=True)
 else:
-    st.error(f"Erro ao acessar o relatório: {response.status_code}")
-
+    # Display an error message if the request was not successful
+    st.error(f"Error accessing the report: {response.status_code}")
