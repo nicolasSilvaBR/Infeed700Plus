d4eed80f7bfa5c8f5fc20dfd54afb169a4ff86f5
nicolasDaSilva <contabit.nicolas@gmail.com>
2024-10-07 21:40:23 +0100

    test session

---
+++ test_session.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# Definir as opções de relatórios
+options = ["Relatório 1", "Relatório 2", "Relatório 3"]
+
+# Inicializar o estado da sessão para garantir que haja um espaço para o valor selecionado
+if 'report_option' not in st.session_state:
+    st.session_state['report_option'] = None
+
+# Criar o primeiro menu de opções
+report_option1 = option_menu(
+    menu_title="Menu 1",
+    menu_icon="reception-4",
+    options=options,
+    icons=["table"] * len(options),
+    default_index=0,  # Não predefinir uma opção
+    key=f"option_menu_1",  # Chave única para o primeiro menu
+)
+
+# Criar o segundo menu de opções
+report_option2 = option_menu(
+    menu_title="Menu 2",
+    menu_icon="reception-4",
+    options=options,
+    icons=["table"] * len(options),
+    default_index=0,  # Não predefinir uma opção
+    key=f"option_menu_2",  # Chave única para o segundo menu  
+)
+
+# Verificar qual menu foi usado para fazer a seleção e atualizar o session_state
+if report_option1 != st.session_state['report_option']:
+    st.session_state['report_option'] = report_option1
+elif report_option2 != st.session_state['report_option']:
+    st.session_state['report_option'] = report_option2
+
+# Lógica para exibir o relatório selecionado
+if st.session_state['report_option']:
+    st.write(f"Você selecionou: {st.session_state['report_option']}")
+else:
+    st.write("Nenhum relatório selecionado.")
