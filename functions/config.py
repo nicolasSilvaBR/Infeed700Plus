270f769d64c4e029c0200db71c56b39c6dbfcbee
ndasilva <contabit.nicolas@gmail.com>
2024-11-01 14:49:55 +0000

    Page name

--- functions/config.py
+++ functions/config.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
+
 def set_page_config():
     """Set page configuration for Streamlit app."""
-
+    
+    if "selected-project" not in st.session_state:
+        st.session_state["selected-project"] = 'Infeed700'
+        
+    project_name = st.session_state["selected-project"]
+    site_name = st.secrets['site_info']['site_name']
+   
+    
     st.set_page_config(
-        page_title="Infeed700 Reports",
+        page_title=f"{project_name} Reports - {site_name}",
         page_icon="📊",
         layout="wide",
         initial_sidebar_state="expanded",
