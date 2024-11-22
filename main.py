665d195892c5eb719ec8ee27e7d86ba4b5095372
ndasilva <contabit.nicolas@gmail.com>
2024-11-22 17:27:15 +0000

    Login Form

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from functions.database_connection import mydb
 import streamlit.components.v1 as components
 from pathlib import Path
 from functions.utilities import load_local_css, get_base64_image
+from functions.web_app_enabled import IsWebAppEnabled
+from functions.login_form import login_form
 
 # From function folder import config > set_page_config()
 config.set_page_config()
@@ -16,11 +18,13 @@ config.set_page_config()
 # Load custom CSS
 load_local_css("assets/css/style.css")
 
+# Create a database connection engine
+engine = mydb()
+
 def main():
     # Initial settings and database connection log
-    engine = mydb()
     logging.info(f"Database engine: {engine}")
-    LeftMenu(engine)    
+    LeftMenu(engine)
     
     # Initialize session variables
     if 'selected_report' not in st.session_state:
@@ -34,22 +38,17 @@ def main():
     minDate = st.session_state.get('minDate', pd.to_datetime("2024-10-01")).strftime('%Y-%m-%d')
     maxDate = st.session_state.get('maxDate', pd.to_datetime("2024-10-30")).strftime('%Y-%m-%d')
     
-    # st.write(st.session_state['selected-project'])
-    # st.write(st.session_state['selected_header'])
-    # st.write(st.session_state['selected_report'])  
-    
     # Function to display the SSRS report
     def display_ssrs_report():
         """Display the SSRS report."""
         try:
-            #with st.spinner('Running Report...'):
-                reportRDLname = st.session_state['selected_report']
-                embed_ssrs_report(reportRDLname, minDate, maxDate)                
+            reportRDLname = st.session_state['selected_report']
+            embed_ssrs_report(reportRDLname, minDate, maxDate)
         except Exception as e:
             if st.session_state['selected_report']:
                 logging.error(f"Error loading report: {e}")
-                st.error("An error occurred while loading the report. Verify that the SSRS server is online.") 
-            elif not st.session_state['selected_report']:
+                st.error("An error occurred while loading the report. Verify that the SSRS server is online.")
+            else:
                 st.write("💬 Please select a Category and Report from the left menu.")
 
     # Function to display the dashboard
@@ -63,7 +62,7 @@ def main():
     # Update `show_content` and `show_report` based on the selected report
     if st.session_state['selected_report'] is not None:
         st.session_state['show_report'] = True
-        st.session_state['show_content'] = True # Show SSRS Report
+        st.session_state['show_content'] = True  # Show SSRS Report
 
     # Display the home (index.html) only if `show_content` is False
     if not st.session_state['show_content']:
@@ -83,20 +82,29 @@ def main():
         index_html = index_html.replace("assets/images/home_silo.png", image_url)
         components.html(index_html, height=900, scrolling=True) 
     
-    # If  report header was selected, display specific content
-    else:  
-        # Show the SSRS report if a report header was selected
+    # If report header was selected, display specific content
+    else:
         if st.session_state['show_report']:
-            #st.write('Show the SSRS report if a report header was selected')
-            if st.session_state['selected-project'] == 'Infeed700' or st.session_state['selected-project'] == 'Enecoms':
+            if st.session_state['selected-project'] in ['Infeed700', 'Enecoms']:
                 display_ssrs_report()
             elif st.session_state['selected-project'] == 'Python':
                 display_dashboard()
 
     # Close main div
-    st.markdown('</div>', unsafe_allow_html=True)    
-    
+    st.markdown('</div>', unsafe_allow_html=True)
+
 # Entry point of the application
 if __name__ == '__main__':
-    main()
+    # Initialize login state
+    if 'is_logged_in' not in st.session_state:
+        st.session_state['is_logged_in'] = False
 
+    # Check if login is required
+    if IsWebAppEnabled(engine) == '1':  # Login required
+        if not st.session_state['is_logged_in']:
+            if login_form():  # Successful login
+                st.session_state['is_logged_in'] = True                
+        if st.session_state['is_logged_in']:
+            main()  # Display the main app
+    else:  # Login not required
+        main()
